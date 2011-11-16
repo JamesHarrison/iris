fad9760df309a08de5a269e25e5279e109f48b41
James Harrison <james@balvedastudios.com>
2011-11-16 01:55:48 +0000

    Minor change to support more recent libebur128 loudness tool. This will break systems with older libebur128 installs. Upgrade that and then IRIS.

--- lib/r128.rb
+++ lib/r128.rb
@@ -1,16 +1,16 @@
-# Very simple class to use r128-scanner from within Ruby to get whole-track LUFS, LRA, sample/true peak/true peak dBTP, as well as momentary waveform data
+# Very simple class to use libebur128's loudness tool from within Ruby to get whole-track LUFS, LRA, sample/true peak/true peak dBTP, as well as momentary waveform data
 class R128
   def self.scan(path)
     print "Scanning #{path}"
     out = ''
-    IO.popen(['r128-scanner', '-p', 'all', '-l', path]){|io|out = io.read}
+    IO.popen(['loudness', 'scan', '-p', 'all', '-l', path]){|io|out = io.read}
     print "Got: #{out}"
     d = out.split("\n")[0].split(",")
     return {:lufs=>d[0].to_f, :lra=>d[1].to_f, :sample_peak=>d[2].to_f, :true_peak_float=>d[3].to_f, :true_peak_dbtp=>d[4].to_f}
   end
   def self.momentary(path,interval=0.4)
     out = ''
-    IO.popen(['r128-scanner', '-m', interval.to_s, path]){|io|out = io.read}
+    IO.popen(['loudness', 'dump', '-m', interval.to_s, path]){|io|out = io.read}
     d = out.split("\n").map{|v|v.gsub("\n","").strip}
     d.delete("-inf")
     return d.map{|v|v.to_f}
